3fe3a00db4272d0dfa8a902fa32146486c059936
SubhojitBh <bhattacherjeesubhojit@gmail.com>
2025-04-05 20:07:19 +0530

    lastfr

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/aws', (req, res) => {
     `);
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
     console.log(`hosting at localhost:${port}`);
 });
